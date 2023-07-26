da19e95b7bc6f4f4f06d441bd4468eef81ac267d
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-07-26 09:40:39 -0300

    update
    update

--- app/envio-automatico.py
+++ app/envio-automatico.py
@@ -7,14 +7,14 @@ import ctypes
 
 def envio_automatico():
     clientes()
-    time.sleep(3)
+    time.sleep(5)
     estoques()
-    time.sleep(3)
+    time.sleep(5)
     produtos()
-    time.sleep(3)
+    time.sleep(5)
     vendas()
-    time.sleep(3)
-    
+    time.sleep(5)
+
     ctypes.windll.user32.MessageBoxW(0, "Envio de arquivos McCain realizado!", "Automação McCain", 1)
 
 if __name__ == "__main__":
