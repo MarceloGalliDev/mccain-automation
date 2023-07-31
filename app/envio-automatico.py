630cdb96a44212a930ff2d647b4a7166a9919354
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-07-31 09:11:55 -0300

    update
    update

--- app/envio-automatico.py
+++ app/envio-automatico.py
@@ -4,6 +4,13 @@ from produtos import produtos
 from vendas import vendas
 import time
 import ctypes
+import threading
+
+MB_OK = 0x0
+TIMEOUT = 5000
+
+def show_message_box():
+    ctypes.windll.user32.MessageBoxW(0, "Envio de arquivos McCain realizado!", "Automação McCain", MB_OK)
 
 def envio_automatico():
     clientes()
@@ -14,8 +21,15 @@ def envio_automatico():
     time.sleep(5)
     vendas()
     time.sleep(5)
+    
+    t = threading.Thread(target=show_message_box)
+    t.start
+    
+    time.sleep(TIMEOUT / 1000.0)
 
-    ctypes.windll.user32.MessageBoxW(0, "Envio de arquivos McCain realizado!", "Automação McCain", 1)
-
+    hwnd = ctypes.windll.user32.FindWindowW(None, "Automação McCain")
+    if hwnd != 0 :
+        ctypes.windll.user32.SendMessageW(hwnd, 0x0010, 0, 0)
+    
 if __name__ == "__main__":
     envio_automatico()
